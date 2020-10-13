4ebf4ec04b2898ccebcc913c81ea30435779fe26
liamzety <58632095+liamzety@users.noreply.github.com>
2020-10-13 12:52:21 +0300

    m

--- server.js
+++ server.js
@@ -8,13 +8,12 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3030;
 const http = require('http').createServer(app);
-// const io = require('socket.io')(http);
 
 // Express App Config
 app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(session({
-    secret: 'puki muki secret stuff',
+    secret: 'ahla bahla shebaolam',
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false }
@@ -30,21 +29,9 @@ if (process.env.NODE_ENV === 'production') {
     app.use(cors(corsOptions));
 }
 
-const authRoutes = require('./api/auth/auth.routes')
-const addTemplateRoutes = require('./api/template/template.route')
-const addUserRoutes = require('./api/user/user.route')
-// const connectSockets = require('./api/socket/socket.routes')
-
-
-// routes
-app.use('/api/auth', authRoutes)
-app.use('/api/template', addTemplateRoutes)
-app.use('/api/user', addUserRoutes)
-// connectSockets(io)
-
-// app.get('/**', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// })
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+})
 
 http.listen(port, () => {
     (`listening on http://localhost:${port}`)
