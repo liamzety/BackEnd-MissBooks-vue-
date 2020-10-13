0eefc1049267560d5789517f7c169cfdb6b5cd98
liamzety <58632095+liamzety@users.noreply.github.com>
2020-10-13 12:55:12 +0300

    m

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(session({
 }))
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve(__dirname, 'public')));
+    app.use(express.static(path.resolve(__dirname, 'dist')));
 } else {
     const corsOptions = {
         origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
